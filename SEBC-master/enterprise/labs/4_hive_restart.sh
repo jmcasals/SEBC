79c06e869f59051e5ec74f172f9a79677fc416d6
jmcasals <josepmaria.casals@sdggroup.com>
2017-03-08 11:36:49 +0100

    Update 4_hive_restart.sh

---
+++ SEBC-master/enterprise/labs/4_hive_restart.sh
@@ -0,0 +1,15 @@
+Version: Cloudera Enterprise Data Hub Edition Trial 5.10.0 (#85 built by jenkins on 20170120-1037 git: aa0b5cd5eceaefe2f971c13ab657020d96bb842a) 
+
+Java VM Name: Java HotSpot(TM) 64-Bit Server VM
+
+Java VM Vendor: Oracle Corporation
+
+Java Version: 1.7.0_67
+
+Server Time: Mar 8, 2017 5:21:24 AM, Eastern Standard Time (EST)
+
+Copyright © 2011-2017 Cloudera, Inc. All rights reserved.
+Hadoop and the Hadoop elephant logo are trademarks of the Apache Software Foundation.
+
+------------------------------------------------------------------------
+Unfortunately I cannot perform any uopgrade ad I do have the latest version
